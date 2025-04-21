509c0d0978d492466f409defb8c10e70d4f6cfb7
svaditya18 <158244922+svaditya18@users.noreply.github.com>
2025-04-21 19:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def load_data_and_models():
         df = pd.read_csv(csv_url, encoding='utf-8',on_bad_lines='skip')
     except UnicodeDecodeError:
         try:
-           csv_url = "https://github.com/svaditya18/Book-Recommendation-System/blob/main/data/book_details.csv"
+            csv_url = "https://github.com/svaditya18/Book-Recommendation-System/blob/main/data/book_details.csv"
             df = pd.read_csv(csv_url, encoding='latin1',on_bad_lines='skip')
         except Exception as e:
             st.error(f"Failed to read CSV file: {str(e)}")
